e38c79aec30acfc9d76aa72ae35a095c4d10a682
raarchell <racheliaptradr@gmail.com>
2021-02-23 22:15:32 +0700

    ga tau apa

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Admin</title>
-        @include('includes.admin.styles')
-    </head>
-    <body class="sb-nav-fixed">
-        @include('includes.admin.navbar')
-        <div id="layoutSidenav">
-            @include('includes.admin.sidebar')
-            <div id="layoutSidenav_content">
-                <main>
-                    @yield('content')
-                </main>
-                @include('includes.admin.footer')
-            </div>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Admin</title>
+    @include('includes.admin.styles')
+</head>
+
+<body class="sb-nav-fixed">
+    @include('includes.admin.navbar')
+    <div id="layoutSidenav">
+        @include('includes.admin.sidebar')
+        <div id="layoutSidenav_content">
+            <main>
+                @yield('content')
+            </main>
+            @include('includes.admin.footer')
         </div>
-        @include('includes.admin.script')
-    </body>
+    </div>
+    @include('includes.admin.script')
+</body>
+
 </html>
